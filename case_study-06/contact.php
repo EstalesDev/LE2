08a428bfbdffaccf5da671cba40e19df76546a50
EstalesDev <ashlyestales2@gmail.com>
2026-06-28 22:48:30 +0800

    Update contact.php

--- case_study-06/contact.php
+++ case_study-06/contact.php
@@ -1,4 +1,24 @@
 <h1>Contact Us</h1>
+
+<p>
+Have questions or concerns? Fill out the form below and we will get back to you as soon as possible.
+</p>
+
+<form action="contact_submit.php" method="post">
+
+    <label>Email</label><br>
+    <input type="email" name="email" placeholder="Enter your email" required><br>
+
+    <label>Subject</label><br>
+    <input type="text" name="subject" placeholder="Enter the subject" required><br>
+
+    <label>Content of Letter</label><br>
+    <textarea name="content" placeholder="Write your message here..." required></textarea><br>
+
+    <input class="button" type="submit" value="Submit">
+
+</form>
+
 <p>
 You can reach us using the following details:
 </p>
